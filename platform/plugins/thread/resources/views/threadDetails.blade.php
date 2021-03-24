d6d3d23845fa425c8aa7d2687cb656b1d8be9c88
osama.cmpk <osama.cmpk@gmail.com>
2021-03-25 02:53:10 +0500

    Carousel

--- platform/plugins/thread/resources/views/threadDetails.blade.php
+++ platform/plugins/thread/resources/views/threadDetails.blade.php
@@ -1,5 +1,3 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 <?php $thread = $options['data']['thread']; ?>
 <?php $variations = $options['data']['variations']; ?>
 <ul class="nav nav-tabs" id="myTab" role="tablist">
@@ -27,7 +25,7 @@
                         <div class="widget-user-header bg-black">
 
                             <h3 class="widget-user-username widget-title-color-red font-bold profile-name">{{ $thread->designer->first_name.' '.$thread->designer->last_name }}</h3>
-                            <h5 class="widget-user-desc text-light">Designer</h5>
+                            <h5 class="widget-user-desc text-dark">Designer</h5>
 
                         </div>
                     </div>
@@ -325,55 +323,36 @@
                     <div class="style_specification">
                         <div class="row ">
                             <div class="col-md-4 ">
-                                <h4>Style</h4>
-                                <div class="stylebox table-responsive">
-                                    <table>
-                                        <tbody>
-                                        <tr>
-                                            <td>
-                                                @if(count($thread->spec_files))
-                                                <div class="container">
-                                                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                                                        <!-- Indicators -->
-                                                        <ol class="carousel-indicators">
-                                                            @foreach($thread->spec_files as $specfile)
-                                                                <li data-target="#myCarousel" data-slide-to="{{ $loop->iteration - 1 }}"
-                                                                    class="{{ $loop->first ? 'active' : '' }}"></li>
-                                                            @endforeach
-                                                        </ol>
+                                <h4>Style</h4>  
+                                <div class="slideshow-container mt-4">
 
-                                                        <!-- Wrapper for slides -->
-                                                        <div class="carousel-inner">
-                                                            @foreach($thread->spec_files as $specfile)
-                                                                <div class="item {{ $loop->first ? 'active' : '' }}">
-                                                                    <img
-                                                                        src="{{ asset($specfile->spec_file) }}"
-                                                                        alt="{{ $thread->name }}" style="width:100%;">
-                                                                </div>
-                                                            @endforeach
-                                                        </div>
-
-                                                        <!-- Left and right controls -->
-                                                        <a class="left carousel-control" href="#myCarousel"
-                                                           data-slide="prev">
-                                                            <span class="glyphicon glyphicon-chevron-left"></span>
-                                                            <span class="sr-only">Previous</span>
-                                                        </a>
-                                                        <a class="right carousel-control" href="#myCarousel"
-                                                           data-slide="next">
-                                                            <span class="glyphicon glyphicon-chevron-right"></span>
-                                                            <span class="sr-only">Next</span>
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                                @endif
-
-                                            </td>
-                                        </tr>
-                                        </tbody>
-                                    </table>
+                                <div class="mySlides fade">
+                                <div class="numbertext">1 / 3</div>
+                                <img src="https://www.w3schools.com/howto/img_nature_wide.jpg" style="width:100%">
+                                <div class="text">Caption Text</div>
                                 </div>
-                            </div>
+
+                                <div class="mySlides fade">
+                                <div class="numbertext">2 / 3</div>
+                                <img src="https://www.w3schools.com/howto/img_nature_wide.jpg" style="width:100%">
+                                <div class="text">Caption Two</div>
+                                </div>
+
+                                <div class="mySlides fade">
+                                <div class="numbertext">3 / 3</div>
+                                <img src="https://www.w3schools.com/howto/img_nature_wide.jpg" style="width:100%">
+                                <div class="text">Caption Three</div>
+                                </div>
+
+                                </div>
+                                <br>
+
+                                <div style="text-align:center">
+                                <span class="dot"></span> 
+                                <span class="dot"></span> 
+                                <span class="dot"></span> 
+                                </div>
+                             </div>
                             <div class="col-md-8">
                                 @if($thread->is_denim == 1)
                                     <div class="specificationwrap">
@@ -578,7 +557,98 @@
 
     .main-form {
         width: 125% !important;
-    }
+    } 
+
+
+    .mySlides {display: none;}
+img {vertical-align: middle;}
+
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active-slider {
+  background-color: #717171;
+}
+
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 1.5s;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@-webkit-keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .text {font-size: 11px}
+}
 </style>
 
+
+<script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active-slider", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active-slider";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+</script>
 
