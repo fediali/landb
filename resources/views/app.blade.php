238b5a33fa42816b636d6037a41ccd1c6019b3c3
talhamubashar0012 <talhatalha0012@gmail.com>
2021-04-06 13:15:54 +0500

    html routes for usama

---
+++ resources/views/app.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+
+</head>
+<body>
+
+@yield('content')
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::group(['prefix' => 'html'], function () {
+  Route::get('/home', function (){
+    return view('index');
+  });
+});
