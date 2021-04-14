e20ed1bcee416d68d6a49dfb505660938a6b12a4
Fedi <farhad.ali@landbapparel.com>
2021-04-14 16:25:02 -0500

    #sku fix

--- platform/plugins/ecommerce/src/Providers/EcommerceServiceProvider.php
+++ platform/plugins/ecommerce/src/Providers/EcommerceServiceProvider.php
@@ -446,7 +446,7 @@ class EcommerceServiceProvider extends ServiceProvider
                     'parent_id'   => 'cms-plugins-ecommerce',
                     'name'        => 'Order Import',
                     'icon'        => 'fa fa-file-import',
-                    'url'         => route('orders.index'),
+                   // 'url'         => route('orders-import.index'),
                     'permissions' => ['orders.import'],
                 ])
                 ->registerItem([
