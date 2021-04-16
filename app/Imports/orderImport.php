bbba527c77edf3fcbf7b5f7563e04ad1801c0fb8
Fedi <farhad.ali@landbapparel.com>
2021-04-16 15:04:24 -0500

    #ff

--- app/Imports/orderImport.php
+++ app/Imports/orderImport.php
@@ -31,7 +31,7 @@ class orderImport implements ToModel, WithHeadingRow
     public function model(array $row)
     {
 //checking customer
-        dd($row);
+
         $customer = Customer::where(['phone' => $row['phone_number']])->first();
         if ($customer == null) {
             //creating Customer
@@ -45,19 +45,25 @@ class orderImport implements ToModel, WithHeadingRow
             $detail['type'] = Order::LASHOWROOM;
             CustomerDetail::create($detail);
             //creating address
-            $baddress['billing_address'] = $row['address'];
-            $baddress['billing_city'] = $row['address'];
-            $baddress['billing_state'] = $row['address'];
-            $baddress['billing_zip_code'] = $row['address'];
-            $baddress['billing_county'] = $row['address'];
+            $baddress['address'] = $row['billing_address'];
+            $baddress['city'] = $row['billing_city'];
+            $baddress['state'] = $row['billing_state'];
+            $baddress['zip_code'] = $row['billing_zip_code'];
+            $baddress['customer_id'] = $customer->id;
+            $baddress['phone'] = $row['phone_number'];
+            $baddress['country'] = $row['billing_county'];
+            $baddress['name'] = $row['business_contact_name'];
 
             $billing = Address::create($baddress);
 
-            $saddress['shipping_address'] = $row['address'];
-            $saddress['shipping_city'] = $row['address'];
-            $saddress['shipping_state'] = $row['address'];
-            $saddress['shipping_zip_code'] = $row['address'];
-            $saddress['shipping_country'] = $row['address'];
+            $saddress['address'] = $row['shipping_address'];
+            $saddress['city'] = $row['shipping_city'];
+            $saddress['state'] = $row['shipping_state'];
+            $saddress['zip_code'] = $row['shipping_zip_code'];
+            $saddress['country'] = $row['shipping_country'];
+            $saddress['customer_id'] = $customer->id;
+            $saddress['phone'] = $row['phone_number'];
+            $baddress['name'] = $row['shipping_contact_name'];
 
             $shipping = Address::create($saddress);
 
@@ -69,27 +75,36 @@ class orderImport implements ToModel, WithHeadingRow
         $pack = quantityCalculate($product->category_id);
         $orderQuantity = $row['original_qty'] / $pack;
 
+        $orderPo = DB::table('ec_order_import')->where('po_number', $row['po'])->get();
+        if ($orderPo->count() > 0) {
+            $detail['order_id'] = $orderPo->order_id;
+            $detail['qty'] = $pack;
+            $detail['price'] = $row['sub_total'];
+            $detail['product_id'] = $product->id;
+            $detail['product_name'] = $product->name;
+            OrderProduct::create($detail);
+            //import record
+        } else {
+            $order['customer_id'] = $customer->id;
+            $order['amount'] = $row['original_amount'];
+            $order['currency_id'] = $customer->id;
+            $order['is_confirmed'] = 1;
+            $order['is_finished'] = 1;
+
+            $importOrder = Order::create($order);
+            $detail['order_id'] = $importOrder->id;
+            $detail['qty'] = $pack;
+            $detail['price'] = $row['sub_total'];
+            $detail['product_id'] = $product->id;
+            $detail['product_name'] = $product->name;
+            OrderProduct::create($detail);
+            //import record
+            $orderInfo['order_id'] = $importOrder->id;
+            $orderInfo['po_number'] = $row['po'];
+            $orderInfo['order_date'] = $row['order_date'];
+
+        }
         //creating Order
-        $order['customer_id'] = $customer->id;
-        $order['amount'] = $row['original_amount'];
-        $order['currency_id'] = $customer->id;
-        $order['is_confirmed'] = 1;
-        $order['is_finished'] = 1;
-
-        $importOrder = Order::create($order);
-        $detail['order_id'] = $importOrder->id;
-        $detail['qty'] = $pack;
-        $detail['price'] = $row['sub_total'];
-        $detail['product_id'] = $product->id;
-        $detail['product_name'] = $product->name;
-        OrderProduct::create($detail);
-        //import record
-        $orderInfo['order_id'] = $importOrder->id;
-        $orderInfo['po_number'] = $row['po'];
-        $orderInfo['order_date'] = $row['order_date'];
-
-
-        dd($pack);
 
 
     }
