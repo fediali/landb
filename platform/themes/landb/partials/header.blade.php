6149ef96b705bef6e87276e5fd14b788c06ed7d6
osama.cmpk <osama.cmpk@gmail.com>
2021-04-27 02:10:24 +0500

    Counter, Animation, Search

--- platform/themes/landb/partials/header.blade.php
+++ platform/themes/landb/partials/header.blade.php
@@ -60,11 +60,26 @@
             </ul>
             <ul class="d-flex">
                 <li>
-                    <a href="#"><i class="fal fa-search"></i></a>
+                    <a href="#" onclick="openSearch()"><i class="fal fa-search"></i></a>
                 </li>
                 <li>
                     <a href="{{ route('public.cart_index') }}"><i class="fal fa-shopping-cart"></i></a>
                 </li>
+                <span style="
+                    background: black;
+                    color: white;
+                    /* position: absolute; */
+                    margin-left: -15px;
+                    height: 20px;
+                    width: 20px;
+                    text-align: center;
+                    border-radius: 30px;
+                    font-size: 12px;
+                    /* vertical-align: -webkit-baseline-middle; */
+                    /* display: inherit; */
+                    margin: a;
+                    margin-top: -10px;
+                ">1</span>
                 <li>
                     <a href="#"><i class="fal fa-user"></i></a>
                 </li>
@@ -109,3 +124,90 @@
 <div class="loading-overlay">
     <span class="fas fa-spinner fa-3x fa-spin"></span>
 </div>
+
+<div id="myOverlay" class="overlay">
+  <span class="closebtn" onclick="closeSearch()" title="Close Overlay">×</span>
+  <div class="overlay-content">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit"><i class="fa fa-search"></i></button>
+    </form>
+  </div>
+</div>
+
+<style> 
+* {
+  box-sizing: border-box;
+}
+  
+.overlay {
+  height: 100%;
+  width: 100%;
+  display: none;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: rgb(0,0,0);
+  background-color: rgba(0,0,0, 0.9);
+}
+
+.overlay-content {
+  position: relative;
+  top: 46%;
+  width: 80%;
+  text-align: center;
+  margin-top: 30px;
+  margin: auto;
+}
+
+.overlay .closebtn {
+  position: absolute;
+  top: 20px;
+  right: 45px;
+  font-size: 60px;
+  cursor: pointer;
+  color: white;
+}
+
+.overlay .closebtn:hover {
+  color: #ccc;
+}
+
+.overlay input[type=text] {
+  padding: 15px;
+  font-size: 17px;
+  border: none;
+  float: left;
+  width: 80%;
+  background: white;
+}
+
+.overlay input[type=text]:hover {
+  background: #f1f1f1;
+}
+
+.overlay button {
+  float: left;
+  width: 20%;
+  padding: 15px;
+  background: #ddd;
+  font-size: 17px;
+  border: none;
+  cursor: pointer;
+}
+
+.overlay button:hover {
+  background: #bbb;
+}
+</style>
+
+<script>
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
+</script>
