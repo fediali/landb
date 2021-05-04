572c4b6cf10393c207b31db8718f782c5214a65c
osama.cmpk <osama.cmpk@gmail.com>
2021-05-05 01:38:00 +0500

    Invoice

--- platform/themes/landb/views/orderSuccess.blade.php
+++ platform/themes/landb/views/orderSuccess.blade.php
@@ -1,1 +1,212 @@
-<h2>Order completed</h2>
+<section class="ml-5 mr-5 mt-3">
+        <div class="row">
+        <div class="col-lg-9">
+            <img src="./img/Logo.png" />
+        </div>
+        <div class="col-lg-3">
+            <p>
+           <b> ORDER # 105768 </b>
+            </p>
+            <p>
+                <b>ORDER DATE</b> 05/01/2021, 08:54 AM  
+            </p>
+            <p>
+                <b>PAYMENT</b> UPS Shipping  
+            </p>
+            <p>
+                <b>STATUS</b> Pick up 
+            </p>
+
+        </div>
+            
+        </div>
+        <div class="row mt-5"> 
+            <div class="col-lg-4">
+                <div style="background: #eaeaea;" class="p-3">
+                <h3>
+                    <b>STORE</b>
+                     </h3>
+                     <hr style="border: 2px solid;">
+                <p>
+                    12801 N, Stemmons Fwt, Suite 710 Farmers Branch, Texas 78865 United States
+                </p>
+                <p>
+                    97251235552 
+                </p>
+                <p>
+                    customerservice@landapparel.com
+                </p>
+                <p>
+                    https://landapparel.com/
+                </p>
+            </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="p-3">
+                <h3>
+                    <b>BILL TO</b>
+                     </h3>
+                     <hr style="border: 2px solid #DDD;">
+                <p>
+                    12801 N, Stemmons Fwt, Suite 710 Farmers Branch, Texas 78865 United States
+                </p>
+                <p>
+                    97251235552 
+                </p>
+                <p>
+                    customerservice@landapparel.com
+                </p>
+                <p>
+                    https://landapparel.com/
+                </p>
+            </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="p-3">
+                <h3>
+                    <b>SHIP TO</b>
+                     </h3>
+                     <hr style="border: 2px solid #DDD;">
+                <p>
+                    12801 N, Stemmons Fwt, Suite 710 Farmers Branch, Texas 78865 United States
+                </p>
+                <p>
+                    97251235552 
+                </p>
+                <p>
+                    customerservice@landapparel.com
+                </p>
+                <p>
+                    https://landapparel.com/
+                </p>
+            </div>
+            </div>  
+            </div>
+    </section>
+    <section class="shoplisting_wrap pl-5 pr-5 mbtb-pl-2 mbtb-pr-2">
+    <hr style="border: 2px solid;">
+
+        <div class="row d-mbtb-none">
+            <div class="col-lg-7 mt-2 mb-3 text-center">
+                <p class="cart-head">Item Description</p>
+            </div>
+            <div class="col-lg-1 mt-2 mb-3">
+                <p class="cart-head">Quantity</p>
+
+            </div>
+            <div class="col-lg-1 mt-2 mb-3 text-center">
+                <p class="cart-head">Price</p>
+
+            </div>
+           
+            <div class="col-lg-1 mt-2 mb-3 text-center">
+                <p class="cart-head"> Discount</p>
+
+            </div>
+            <div class="col-lg-1 mt-2 mb-3 text-center">
+                <p class="cart-head">Tax</p>
+
+            </div>
+            <div class="col-lg-1 mt-2 mb-3">
+                <p class="cart-head">Item Total</p>
+
+            </div>
+        </div>
+        <div class="row mb-4 mt-4">
+            <div class="col-lg-7 mt-2">
+                <div class="d-flex">
+                    <img style="height: 95px; width: 75px;" src="./img/product/Front.png" />
+                    <div class="ml-3">
+                        <p class="cart-product-name mt-2 mb-2">Coral Cut Out V-neck Basic Tee Plus Size</p>
+                        <p class="cart-product-code mb-2">CODE: MH0315-RSER</p>
+                        <p class="cart-product-size">SIZE: 2(XL), 2(2XL), 2(3XL) </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-1 mt-2 text-center">
+                <p class="mt-2">23</p>
+            </div>
+            <div class="col-lg-1 mt-2">
+                <p class="mt-2">$ 25.00</p>
+            </div>
+            <div class="col-lg-1 mt-2 mb-txt-center text-center">
+                <p class="mt-2">-</p>
+            </div>
+            <div class="col-lg-1 mt-2 text-center">
+                <p class="mt-2">-</p>
+            </div>
+            <div class="col-lg-1 mt-2">
+                <p class="mt-2">$ 25.00</p>
+            </div>
+        </div>
+        <hr>
+        <div class="row mb-4 mt-4">
+            <div class="col-lg-7 mt-2">
+                <div class="d-flex">
+                    <img style="height: 95px; width: 75px;" src="./img/product/Front.png" />
+                    <div class="ml-3">
+                        <p class="cart-product-name mt-2 mb-2">Coral Cut Out V-neck Basic Tee Plus Size</p>
+                        <p class="cart-product-code mb-2">CODE: MH0315-RSER</p>
+                        <p class="cart-product-size">SIZE: 2(XL), 2(2XL), 2(3XL) </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-1 mt-2 text-center">
+                <p class="mt-2">23</p>
+            </div>
+            <div class="col-lg-1 mt-2">
+                <p class="mt-2">$ 25.00</p>
+            </div>
+            <div class="col-lg-1 mt-2 mb-txt-center text-center">
+                <p class="mt-2">-</p>
+            </div>
+            <div class="col-lg-1 mt-2 text-center">
+                <p class="mt-2">-</p>
+            </div>
+            <div class="col-lg-1 mt-2">
+                <p class="mt-2">$ 25.00</p>
+            </div>
+        </div> 
+
+        <hr style="border: 2px solid;">
+        <div class="row">
+            <div class="col-lg-12">
+                <p class="cart-product-name mt-2 mb-2">Gift Certificate</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 col-6">
+                <p class="mt-2">Subtotal</p>
+            </div>
+            <div class="col-lg-6 col-6 text-right">
+                <p class="mt-2">$ 40.00</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-6 col-6">
+                <p class="mt-2">Shipping</p>
+            </div>
+            <div class="col-lg-6 col-6 text-right">
+                <p class="mt-2">$ 40.00</p>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-lg-6 col-6">
+                <h3>
+                    <b>TOTAL</b>
+                     </h3>
+            </div>
+            <div class="col-lg-6 col-6 text-right">
+                <h3>
+                    <b>$ 40.00</b>
+                     </h3>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col-lg-12 col-12">
+                <p class="mt-2"> For return policy please visit landapparel.com/faq.html
+                </p>
+            </div> 
+        </div>
+    </section>
