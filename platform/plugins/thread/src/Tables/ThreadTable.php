7aee8184d423524f9eb7adf83cc59cad03f752d8
Fedi <farhad.ali@landbapparel.com>
2021-05-11 16:22:07 -0500

    #ff

--- platform/plugins/thread/src/Tables/ThreadTable.php
+++ platform/plugins/thread/src/Tables/ThreadTable.php
@@ -183,7 +183,7 @@ class ThreadTable extends TableAbstract
                 'name'    => 'threads.status',
                 'title'   => trans('core/base::tables.status'),
                 'width'   => '100px',
-                'visible' => (Auth::user()->hasPermission(['thread.create'])) ? true : false,
+//                'visible' => (Auth::user()->hasPermission(['thread.create'])) ? true : false,
             ],
             'ready'               => [
                 'name'    => 'threads.ready',
