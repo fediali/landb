ae97c9573c85fca1dc254a2065d7a628740ea540
shakirmirza-bv <shakir@bargoventures.com>
2021-05-28 03:46:03 +0500

    ff

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -14,8 +14,9 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Broadcast::routes();
-        Broadcast::routes(['middleware' => ['auth:api']]);
+        Broadcast::routes();
+        // Broadcast::routes(['middleware' => ['auth:api']]);
+        // Broadcast::routes(['middleware' => 'auth:admin']);
 
         require base_path('routes/channels.php');
     }
