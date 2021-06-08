061a2df332a6a17de035c17fd196fdc1f09b75d5
Fedi <farhad.ali@landbapparel.com>
2021-06-08 15:15:08 -0500

    #UI

--- platform/themes/landb/views/product.blade.php
+++ platform/themes/landb/views/product.blade.php
@@ -13,6 +13,7 @@
 </section>
 <section class="shoplisting_wrap">
     <div class="row">
+
         <div class="col-lg-1">
             @if(count($product->images))
                 @foreach($product->images as $image)
@@ -38,67 +39,77 @@
             <h1 class="detail-h1 mb-2"> {{ $product->name }}</h1>
             <p class="detail-price mb-2">$ {{ $product->price }}</p>
             <p class="short-description mb-2">{!! $product->description !!} </p>
-            <p class="detail-size-p mb-2"><span class="detail-size">Size</span>@if(isset($product->category)) @foreach($product->category->category_sizes as $cat_size) {{ $cat_size->name }} {!! ($loop->last) ? '':',' !!} @endforeach @endif </p>
+            <p class="detail-size-p mb-2"><span
+                    class="detail-size">Size</span>@if(isset($product->category)) @foreach($product->category->category_sizes as $cat_size) {{ $cat_size->name }} {!! ($loop->last) ? '':',' !!} @endforeach @endif
+            </p>
             <select class="detail-size-select">
                 @if(isset($product->category))
-                @foreach($product->category->category_sizes as $cat_size)
-                    <option value="{{ $cat_size->id }}"> {{ $cat_size->name }} </option>
-                @endforeach
+                    @foreach($product->category->category_sizes as $cat_size)
+                        <option value="{{ $cat_size->id }}"> {{ $cat_size->name }} </option>
+                    @endforeach
                 @endif
             </select>
-            <p class="mt-4 detail-color-text"> Color &nbsp;&nbsp;&nbsp;<span class="detail-color-text-p">Peach</span> </p>
+            <p class="mt-4 detail-color-text"> Color &nbsp;&nbsp;&nbsp;<span class="detail-color-text-p">Peach</span>
+            </p>
             <div class="color-area mt-2">
                 <label class="container-check">
                     <input type="checkbox" checked="checked">
                     <span class="checkmark"></span>
                 </label>
                 <label class="container-check">
-                    <input type="checkbox" >
+                    <input type="checkbox">
                     <span class="checkmark"></span>
                 </label>
                 <label class="container-check">
-                    <input type="checkbox" >
+                    <input type="checkbox">
                     <span class="checkmark"></span>
                 </label>
                 <label class="container-check">
-                    <input type="checkbox" >
+                    <input type="checkbox">
                     <span class="checkmark"></span>
                 </label>
                 <label class="container-check">
-                    <input type="checkbox" >
+                    <input type="checkbox">
                     <span class="checkmark"></span>
                 </label>
             </div>
-                <form class="add_to_cart_form" data-id="{{ $product->id }}" method='POST' action='{{ route('public.cart.add_to_cart') }}'>
-               <div class="row mt-4"> 
-                <div id="myform" class="col-lg-4">
-                    <input type='button' value='-' class='qtyminus' data-update="0" field='quantity' />
-                    <input type='text' name='quantity' value='1' class='qty' readonly />
-                    <input type='button' value='+' class='qtyplus' data-update="0" field='quantity' />
-                </div> 
-                <div class="col-lg-4">
-                    <button class="cart-btn w-100 add-to-cart-button cart-submit" data-id="{{ $product->id }}">Add to cart</button>
+            <form class="add_to_cart_form" data-id="{{ $product->id }}" method='POST'
+                  action='{{ route('public.cart.add_to_cart') }}'>
+                <div class="row mt-4">
+                    <div id="myform" class="col-lg-4">
+                        <input type='button' value='-' class='qtyminus' data-update="0" field='quantity'/>
+                        <input type='text' name='quantity' value='1' class='qty' readonly/>
+                        <input type='button' value='+' class='qtyplus' data-update="0" field='quantity'/>
+                    </div>
+                    <div class="col-lg-4">
+                        <button class="cart-btn w-100 add-to-cart-button cart-submit" data-id="{{ $product->id }}">Add
+                            to cart
+                        </button>
+                    </div>
                 </div>
-               </div>
-                </form>
-            <p class="mt-4 detail-basic">Basic Code &nbsp;&nbsp;&nbsp;<span class="detail-basic-p">{{ $product->sku }}</span> </p>
-            <p class="detail-category mt-2">Category: &nbsp;&nbsp;&nbsp;<span class="detail-category-p mt-2">{{ @$product->category->name }}</span> </p>
+            </form>
+            <p class="mt-4 detail-basic">Basic Code &nbsp;&nbsp;&nbsp;<span
+                    class="detail-basic-p">{{ $product->sku }}</span></p>
+            <p class="detail-category mt-2">Category: &nbsp;&nbsp;&nbsp;<span
+                    class="detail-category-p mt-2">{{ @$product->category->name }}</span></p>
             {{--<p class="detail-tag mt-2">Tag:&nbsp;&nbsp;&nbsp;<span class="detail-tag-p">Pottery</span> </p>--}}
             <div class="d-flex mt-4">
                 <p class="share-text pt-1 mr-2"> Share this items :
                 </p>
-                <a href="#"><img class="social-img ml-2" src="{{ asset('landb/img/icons/snapchat.png') }}" /></a>
-                <a href="#"><img class="social-img ml-2" src="{{ asset('landb/img/icons/facebook.png') }}" /></a>
-                <a href="#"><img class="social-img ml-2" src="{{ asset('landb/img/icons/Twitter.png') }}" /></a>
-                <a href="#"><img class="social-img ml-2" src="{{ asset('landb/img/icons/instagram.png') }}" /></a>
+                <a href="#"><img class="social-img ml-2" src="{{ asset('landb/img/icons/snapchat.png') }}"/></a>
+                <a href="#"><img class="social-img ml-2" src="{{ asset('landb/img/icons/facebook.png') }}"/></a>
+                <a href="#"><img class="social-img ml-2" src="{{ asset('landb/img/icons/Twitter.png') }}"/></a>
+                <a href="#"><img class="social-img ml-2" src="{{ asset('landb/img/icons/instagram.png') }}"/></a>
             </div>
         </div>
     </div>
     <div class="row">
         <div class="col-lg-12 mt-4">
             <ul class="nav nav-tabs tabs-product mt-4">
-                <li><a class="active" data-toggle="tab" href="#home">Description&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
-                <li><a data-toggle="tab" href="#menu1">Product Details&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></li>
+                <li><a class="active" data-toggle="tab" href="#home">Description&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                </li>
+                <li><a data-toggle="tab" href="#menu1">Product Details&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                </li>
                 <li><a data-toggle="tab" href="#menu2">Reviews</a></li>
             </ul>
 
@@ -143,7 +154,7 @@
                 <div id="menu2" class="tab-pane fade">
                     <div class="row mt-4">
                         <div class="col-lg-1">
-                            <img class="review-img" src="./img/review.png" />
+                            <img class="review-img" src="./img/review.png"/>
                         </div>
                         <div class="col-lg-11">
                             <div class="row">
@@ -154,13 +165,16 @@
                                     <a href="#" class="review-reply">Reply</a>
                                 </div>
                             </div>
-                            <p class="review-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est viverra velit enim, semper nibh lacus, hendrerit donec. Tellus eget urna amet, cum scelerisque. Nibh arcu orci consequat diam. Libero fermentum scelerisque nam
-                                amet. In egestas in quisque vestibulum, eu massa adipiscing. Eget est non ut ornare blandit. Ut cras nunc suspendisse leo sed pharetra.</p>
+                            <p class="review-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est viverra
+                                velit enim, semper nibh lacus, hendrerit donec. Tellus eget urna amet, cum scelerisque.
+                                Nibh arcu orci consequat diam. Libero fermentum scelerisque nam
+                                amet. In egestas in quisque vestibulum, eu massa adipiscing. Eget est non ut ornare
+                                blandit. Ut cras nunc suspendisse leo sed pharetra.</p>
                         </div>
                     </div>
                     <div class="row mt-4">
                         <div class="col-lg-1">
-                            <img class="review-img" src="./img/review.png" />
+                            <img class="review-img" src="./img/review.png"/>
                         </div>
                         <div class="col-lg-11">
                             <div class="row">
@@ -171,8 +185,11 @@
                                     <a href="#" class="review-reply">Reply</a>
                                 </div>
                             </div>
-                            <p class="review-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est viverra velit enim, semper nibh lacus, hendrerit donec. Tellus eget urna amet, cum scelerisque. Nibh arcu orci consequat diam. Libero fermentum scelerisque nam
-                                amet. In egestas in quisque vestibulum, eu massa adipiscing. Eget est non ut ornare blandit. Ut cras nunc suspendisse leo sed pharetra.</p>
+                            <p class="review-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est viverra
+                                velit enim, semper nibh lacus, hendrerit donec. Tellus eget urna amet, cum scelerisque.
+                                Nibh arcu orci consequat diam. Libero fermentum scelerisque nam
+                                amet. In egestas in quisque vestibulum, eu massa adipiscing. Eget est non ut ornare
+                                blandit. Ut cras nunc suspendisse leo sed pharetra.</p>
                         </div>
                     </div>
                 </div>
