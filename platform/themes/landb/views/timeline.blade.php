243b58c0a0fb1e489665d18b7fb08a6412811a10
Fedi <farhad.ali@landbapparel.com>
2021-06-17 16:27:47 -0500

    #threadSample

--- platform/themes/landb/views/timeline.blade.php
+++ platform/themes/landb/views/timeline.blade.php
@@ -16,9 +16,9 @@
     {{--        </div>--}}
     {{--    </div>--}}
     <div class="timeline">
-        @foreach(@$product->detail as $row)
+        @foreach($product->detail as $row)
             <div class="container-timeline left {{($loop->iteration % 2 == 0) ? 'right': 'left'}}">
-                <div class="date">{{$product->date}}
+                <div class="date">{{\Carbon\Carbon::createFromDate('')}}
                     <a href="{{$row->product_link}}" class=" btn cart-btn ml-2 timeline-addbtn">Add to Cart</a>
                     <a href="tel:972-243-7860" class="timeline-callbtn btn border-btn ml-2">Call Us</a>
                 </div>
