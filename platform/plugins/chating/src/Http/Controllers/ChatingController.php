3e22260a5bbb744f2ce0055d7b465c595a59d778
Fedi <farhad.ali@landbapparel.com>
2021-06-23 17:27:36 -0500

    #Timeline

--- platform/plugins/chating/src/Http/Controllers/ChatingController.php
+++ platform/plugins/chating/src/Http/Controllers/ChatingController.php
@@ -182,7 +182,7 @@ class ChatingController extends BaseController
         page_title()->setTitle('Chat Room');
         $id = Auth::id();
         $customers = get_customers($id);
-        $twilio = new Client(env('TWILIO_AUTH_SID'), env('TWILIO_AUTH_TOKEN'));
+        //$twilio = new Client(env('TWILIO_AUTH_SID'), env('TWILIO_AUTH_TOKEN'));
 
        // $conversation = $twilio->conversations->v1->conversations('CH9b2aae73a6724dfc90e66a3eab72efad')->messages->read();
 //        dd($conversation[0]->author);
@@ -276,9 +276,9 @@ class ChatingController extends BaseController
 //            ->channels("CH5cd6cb9ec79f4005b1a060780da974d6")
 //            ->delete();
 //
-//      $d =   $twilio->chat->v2->services("IS791f2cbcd4484a3cafda2e2c7200f4d0")
-//                    ->delete();
-//dd($d);
+      $d =   $twilio->chat->v2->services("IS791f2cbcd4484a3cafda2e2c7200f4d0")
+                    ->delete();
+dd($d);
         //Text Message
         //$author = '+13345390661';
         //$body = 'Gand Marwao Gathiye Khaoo';
