fb82ddcb984b07270c6aea4b93d4a250b6070f3e
Fedi <farhad.ali@landbapparel.com>
2021-06-30 16:18:28 -0500

    #ff

--- platform/plugins/thread/resources/views/threadDetails.blade.php
+++ platform/plugins/thread/resources/views/threadDetails.blade.php
@@ -430,7 +430,7 @@
                                         @foreach($thread->regular_product_categories()->get() as $cat)
                                             @foreach($cat->category_sizes as $catSize)
                                                 <div style=" margin: 0px 5px;background: #e8e8e8 !important; padding: 10px  !important;width: 65px  !important; border-radius: 5px  !important;    border: 1px solid #9a9a9a  !important;">
-                                                    <label for="name">{{$catSize->name}}</label>
+                                                    <label for="name">{{$catSize->full_name}}</label>
                                                     <p>{{get_pvt_cat_size_qty($thread->id,$cat->id,$catSize->id)}}</p>
                                                 </div>
                                             @endforeach
