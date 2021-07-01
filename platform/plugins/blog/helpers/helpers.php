81f6744d7f50ed816f9620795ee1b6459610cc01
Fedi <farhad.ali@landbapparel.com>
2021-07-01 11:31:53 -0500

    #ff

--- platform/plugins/blog/helpers/helpers.php
+++ platform/plugins/blog/helpers/helpers.php
@@ -891,7 +891,7 @@ if (!function_exists('packProdQtyCalculate')) {
         $category = ProductCategory::where('id', $id)->first();
         $totalQuantity = 0;
 
-        if (!$category->category_sizes->isEmpty() && isset($category->category_sizes) && $category->category_sizes != null) {
+        if (isset($category->category_sizes)) {
             foreach ($category->category_sizes as $cat) {
                 $quan = substr($cat->full_name, strpos($cat->full_name, "-") + 1);
                 $totalQuantity += $quan;
