693d229e434f786822dc1327c572deca89504c7a
Fedi <farhad.ali@landbapparel.com>
2021-07-12 12:48:38 -0500

    #ff

--- platform/plugins/thread/resources/views/threadDetails.blade.php
+++ platform/plugins/thread/resources/views/threadDetails.blade.php
@@ -673,7 +673,7 @@
                                                                 <div
                                                                     style=" font-weight: 600; font-family: 'Raleway', sans-serif;display: flex;  justify-content: space-between; align-items: baseline;">
                                                                     <label for=""> {{ $fit }}</label>
-                                                                    <input style=" font-weight: 600; font-family: 'Raleway', sans-serif;background-color: #ffffff; background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFAQMAAAC3obSmAAAABlBMVEUAAADw8PC5otm+AAAAAXRSTlMAQObYZgAAABJJREFUCNdj4GAQYFBgcGBoAAACogD5g5VHSAAAAABJRU5ErkJggg==); border-color: #000;   color: #fff;
+                                                                    <input style=" font-weight: 600; font-family: 'Raleway', sans-serif;background-color: #ffffff; background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFAQMAAAC3obSmAAAABlBMVEUAAADw8PC5otm+AAAAAXRSTlMAQObYZgAAABJJREFUCNdj4GAQYFBgcGBoAAACogD5g5VHSAAAAABJRU5ErkJggg==); border-color: #ff0000;   color: #000000;
     cursor: default;  opacity: 1.65 !important;" type="checkbox" type="checkbox"
                                                                            disabled {!! ($key == $thread->fit_id) ? 'checked' : '' !!}>
                                                                 </div>
@@ -695,7 +695,7 @@
                                                                 <div
                                                                     style=" font-weight: 600; font-family: 'Raleway', sans-serif;display: flex;  justify-content: space-between; align-items: baseline;">
                                                                     <label for=""> {{ $rise }}</label>
-                                                                    <input style=" font-weight: 600; font-family: 'Raleway', sans-serif;background-color: #ffffff; background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFAQMAAAC3obSmAAAABlBMVEUAAADw8PC5otm+AAAAAXRSTlMAQObYZgAAABJJREFUCNdj4GAQYFBgcGBoAAACogD5g5VHSAAAAABJRU5ErkJggg==); border-color: #000;   color: #fff;
+                                                                    <input style=" font-weight: 600; font-family: 'Raleway', sans-serif;background-color: #ffffff; background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFAQMAAAC3obSmAAAABlBMVEUAAADw8PC5otm+AAAAAXRSTlMAQObYZgAAABJJREFUCNdj4GAQYFBgcGBoAAACogD5g5VHSAAAAABJRU5ErkJggg==); border-color: #ff0000;   color: #000000;
     cursor: default;  opacity: 1.65 !important;" type="checkbox"
                                                                            disabled {!! ($key == $thread->rise_id) ? 'checked' : '' !!}>
                                                                 </div>
