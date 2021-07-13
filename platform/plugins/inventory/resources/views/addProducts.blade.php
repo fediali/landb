5a88db35129886c58f00410a34eb225530c43191
talhamubashar <talhatalha0012@gmail.com>
2021-07-13 03:39:58 -0700

    fixes

--- platform/plugins/inventory/resources/views/addProducts.blade.php
+++ platform/plugins/inventory/resources/views/addProducts.blade.php
@@ -118,13 +118,14 @@
     });
   })
   .then((barcodePicker) => {
-   /* barcodePicker.on("processFrame", (result) => {
-
-  });*/
     // barcodePicker is ready here, show a message every time a barcode is scanned
     barcodePicker.on("scan", (scanResult) => {
       console.log(scanResult.barcodes[0].data)
         $('#scannerInput').val(scanResult.barcodes[0].data);
+          $('#scannerInput').addClass('loading');
+          $('#product-error').hide();
+          $('#scannerInput').removeClass('is-invalid');
+          get_product_by_barcode(scanResult.barcodes[0].data,$('#scannerInput'));
   });
   });
 
