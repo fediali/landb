5c4a6d535893058cb80121f4243a7a7ba538cf76
Fedi <farhad.ali@landbapparel.com>
2021-07-13 12:15:54 -0500

    #ff

--- public/landb/js/custom.js
+++ public/landb/js/custom.js
@@ -312,12 +312,12 @@ $(document).ready(function () {
         $(".dp-scroll-text p:first-child").removeClass("dp-run-script dp-animate-1").addClass("dp-animate-hide");
         $("p.dp-run-script.dp-animate-4").next().addClass("dp-run-script dp-animate-4");
         $(".dp-run-script").removeClass("dp-animate-1 dp-animate-2 dp-animate-3 dp-animate-4");
-        $('#vslider1').attr("src", "http://revamp.landbw.co/img/pr2.png");
-        $('#vslider2').attr("src", "http://revamp.landbw.co/img/pr2.png");
-        $('#vslider3').attr("src", "http://revamp.landbw.co/img/pr2.png");
-        $('#vslider4').attr("src", "http://revamp.landbw.co/img/pr2.png");
-        $('#vslider5').attr("src", "http://revamp.landbw.co/img/pr2.png");
-        $('#vslider6').attr("src", "http://revamp.landbw.co/img/pr2.png");
+        $('#vslider1').attr("src", "http://revamp.landbw.co/landb/img/pr2.png");
+        $('#vslider2').attr("src", "http://revamp.landbw.co/landb/img/pr2.png");
+        $('#vslider3').attr("src", "http://revamp.landbw.co/landb/img/pr2.png");
+        $('#vslider4').attr("src", "http://revamp.landbw.co/landb/img/pr2.png");
+        $('#vslider5').attr("src", "http://revamp.landbw.co/landb/img/pr2.png");
+        $('#vslider6').attr("src", "http://revamp.landbw.co/landb/img/pr2.png");
 
 
         $.each($('.dp-run-script'), function (index, runscript) {
