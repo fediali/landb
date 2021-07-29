f36758c029b60328e58c3783da7aac51a7cbb976
fediali <farhad.surani@gmail.com>
2021-07-29 18:29:38 -0500

    #ff

--- platform/plugins/ecommerce/src/Http/Controllers/Customers/CustomerController.php
+++ platform/plugins/ecommerce/src/Http/Controllers/Customers/CustomerController.php
@@ -375,7 +375,8 @@ class CustomerController extends BaseController
             'total'             => 1,
             'pre_auth'          => 1
         ];
-        $url = (env(" ") . "charge/");
+
+        $url = (env("OMNI_URL") . "charge/");
         list($response, $info) = omni_api($url, $data, 'POST');
 
         $status = $info['http_code'];
