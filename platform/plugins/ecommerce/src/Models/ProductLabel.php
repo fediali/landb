7a323305b8c49916d3659ab71326ecac07210119
Fedi <farhad.ali@landbapparel.com>
2021-08-04 12:11:27 -0500

    #ff #product label

---
+++ platform/plugins/ecommerce/src/Models/ProductLabel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Botble\Ecommerce\Models;
+
+use Botble\Base\Traits\EnumCastable;
+use Botble\Base\Enums\BaseStatusEnum;
+use Botble\Base\Models\BaseModel;
+
+class ProductLabel extends BaseModel
+{
+    use EnumCastable;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'product_labels';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'status',
+    ];
+
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'status' => BaseStatusEnum::class,
+    ];
+}
