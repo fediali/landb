b3652b3e83d8fa960bcd42a20d3d1df74b514b3e
Fedi <farhad.ali@landbapparel.com>
2021-08-09 13:18:14 -0500

    #ff

--- platform/themes/landb/views/index.blade.php
+++ platform/themes/landb/views/index.blade.php
@@ -67,8 +67,8 @@
                                     {!! image_html_generator(@$product->images[0], null, null, null, true, 'bone') !!}
                                     <span>Latest</span>
                                     <div class="imgoverlay">
-                                        <a href="{!! generate_product_url('detail', $product->id, $product->product_slug)  !!}"><i
-                                                    class="far fa-eye"></i></a>
+                                        <a href="{!! generate_product_url('detail', $product->id, $product->product_slug)  !!}">
+                                            <i class="far fa-eye"></i></a>
                                         @if(auth('customer')->user())
                                             <a class="add-to-wishlist" id="wishlist-icon-{{$product->id}}"
                                                href="{!! generate_product_url('wishlist', $product->id) !!}"
