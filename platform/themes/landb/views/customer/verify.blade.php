862931381dc5f54d0799d9d780749d40a96e4452
osama.cmpk <osama.cmpk@gmail.com>
2021-08-10 00:24:57 +0500

    404 Verify Page

--- platform/themes/landb/views/customer/verify.blade.php
+++ platform/themes/landb/views/customer/verify.blade.php
@@ -1,5 +1,5 @@
-<div class="container">
-    <div class="card" style="width: 50rem;">
+<div class="container mt-5 mb-5">
+    <div class="card">
         <div class="card-body">
             Your Profile is still pending for verification. Try again later!
         </div>
