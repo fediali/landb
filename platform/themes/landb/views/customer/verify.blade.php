b12fda389b3e54cbae5a350af363e8a7f5076c3a
osama.cmpk <osama.cmpk@gmail.com>
2021-08-21 01:00:58 +0500

    Size chart, Verify Page

--- platform/themes/landb/views/customer/verify.blade.php
+++ platform/themes/landb/views/customer/verify.blade.php
@@ -1,7 +1,7 @@
 <div class="container mt-5 mb-5">
-    <div class="card">
-        <div class="card-body text-center">
-            <img style="width: 300px; display: block; margin: auto; margin-bottom:20px;" src="{{ asset('landb/img/verify.png') }}" />
+    <div class="card verify-page">
+        <div class="card-body mt-5 text-center">
+            <img class="verify-img" style="width: 300px; display: block; margin: auto; margin-bottom:20px;" src="{{ asset('landb/img/verify.png') }}" />
             Your profile is pending for verification. You will get email once it approved!
         </div>
     </div>
