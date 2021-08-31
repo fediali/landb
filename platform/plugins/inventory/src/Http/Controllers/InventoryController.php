129d02e9ecb8503dc987dc5620d0cbed0f4417a3
shakirmirza-bv <shakir@bargoventures.com>
2021-09-01 02:03:35 +0500

    ff

--- platform/plugins/inventory/src/Http/Controllers/InventoryController.php
+++ platform/plugins/inventory/src/Http/Controllers/InventoryController.php
@@ -243,6 +243,7 @@ class InventoryController extends BaseController
             ->where(['ec_products.ptype' => 'R'])
             ->where('ec_products.status', '!=', BaseStatusEnum::HIDE)
             ->orderBy('ec_products.sku', 'ASC')
+            ->orderBy('ec_products.name', 'ASC')
             ->orderBy('thread_order_variations.thread_order_id', 'DESC')
             ->get();
         if ($products && count($products) > 1) {
