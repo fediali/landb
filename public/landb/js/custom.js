225e8d616efbb4d1400dc10702a6d91c696d318d
talhamubashar <talhatalha0012@gmail.com>
2021-09-04 01:44:00 -0700

    fix

--- public/landb/js/custom.js
+++ public/landb/js/custom.js
@@ -380,7 +380,7 @@ $(document).ready(function () {
 
 });*/
 
-var mybutton = document.getElementById("myBtn");
+/*var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -391,7 +391,7 @@ function scrollFunction() {
   } else {
     mybutton.style.display = "none";
   }
-}
+}*/
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
