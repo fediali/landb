179645a28c6ac602c1c46728696f75661f644a27
shakirmirza-bv <shakir@bargoventures.com>
2021-09-25 01:39:09 +0500

    ff

--- platform/plugins/ecommerce/resources/views/products/partials/inventory_history_table.blade.php
+++ platform/plugins/ecommerce/resources/views/products/partials/inventory_history_table.blade.php
@@ -61,7 +61,7 @@
                                     <p class="muted">
                                         <small>Status: ThreadOrder &gt; {{ $history->thread_order->status }}</small>
                                     </p>
-                                @elseif(!empty(@$history->order_id))
+                                @elseif(@$history->order_id)
                                     <a href="{{ route('orders.edit', @$history->order->id) }}" target="_blank">Order
                                         #{{ @$history->order->id }}</a>
                                     <p class="muted">
