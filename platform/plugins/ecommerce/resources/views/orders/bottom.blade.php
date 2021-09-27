97d9441074b17b74a6536bcb7442953afa36ccd1
shakirmirza-bv <shakir@bargoventures.com>
2021-09-28 02:43:21 +0500

    ff

--- platform/plugins/ecommerce/resources/views/orders/bottom.blade.php
+++ platform/plugins/ecommerce/resources/views/orders/bottom.blade.php
@@ -1,6 +1,6 @@
 <div class="wrapper-filter pull-right">
     <p>Gross Total : $ {{number_format($data['gross_total'])}}</p>
-    <p>Total Paid Real : $ {{number_format($data['total_paid_real'])}}</p>
+    <p>Total Paid : $ {{number_format($data['total_paid'])}}</p>
     <p>Total Shipping Cost : $ {{number_format($data['total_shipping_cost'])}}</p>
 </div>
 
