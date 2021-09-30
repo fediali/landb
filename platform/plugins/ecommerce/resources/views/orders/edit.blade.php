af3dce6476afb271f2ed4feb2679da42822ed850
shakirmirza-bv <shakir@bargoventures.com>
2021-10-01 01:07:31 +0500

    ff

--- platform/plugins/ecommerce/resources/views/orders/edit.blade.php
+++ platform/plugins/ecommerce/resources/views/orders/edit.blade.php
@@ -940,7 +940,6 @@
                                     <div class="row m-0 pt-4 bg-white">
                                         <div class="col-lg-12 ">
                                             <strong class="mb-2">Paid with Paypal</strong>
-                                            <strong class="mb-2">{{$order->payment->paypal_email}}</strong>
                                             <a href="{{'mailto:'.$order->payment->paypal_email}}">
                                                 <span class="asd">{{$order->payment->paypal_email}}</span>
                                             </a>
