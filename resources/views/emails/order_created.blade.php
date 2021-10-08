c5d01ad642186d724183b90c8ca9f819318fb6e3
osama.cmpk <osama.cmpk@gmail.com>
2021-10-09 00:41:28 +0500

    email meta

--- resources/views/emails/order_created.blade.php
+++ resources/views/emails/order_created.blade.php
@@ -2,7 +2,8 @@
 <html lang="en-US">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 </head>
 
 <body style="font-family:arial;">
