e841b3070499914d8d95699426996dac6fbe4a3e
Fedi <farhad.ali@landbapparel.com>
2021-11-02 09:22:05 -0500

    #ff

--- app/Console/Commands/fetchOrders.php
+++ app/Console/Commands/fetchOrders.php
@@ -44,7 +44,7 @@ class fetchOrders extends Command
      */
     public function handle()
     {
-        DB::connection('mysql2')->table('hw_orders')->where('hw_orders.fetch_status', 0)->orderBy('hw_orders.order_id', 'ASC')->chunk(100,
+        DB::connection('mysql2')->table('hw_orders')->where(['hw_orders.fetch_status' => 0, 'status' => 'AJ'],)->orderBy('hw_orders.order_id', 'ASC')->chunk(100,
             function ($orders) {
                 foreach ($orders as $order) {
                     echo $order->order_id;
@@ -141,25 +141,25 @@ class fetchOrders extends Command
                         $order->status = 'Failed Preauth';
                     }
                     $orderData = [
-                        'id' => $order->order_id,
-                        'user_id' => $order->user_id,
-                        'status' => $order->status,
-                        'order_type' => ($pre) ? Order::PRE_ORDER : Order::NORMAL,
-                        'platform' => $order->location ? $order->location : ($order->online_order ? 'online' : 'backorder'),
-                        'amount' => $order->total,
-                        'currency_id' => 1,
-                        'tax_amount' => 0,
-                        'shipping_amount' => $order->shipping_cost,
-                        'description' => $order->notes,
-                        'discount_amount' => $order->subtotal_discount,
-                        'sub_total' => $order->subtotal,
-                        'is_confirmed' => 1,
-                        'is_finished' => 1,
-                        'salesperson_id' => $order->issuer_id,
-                        'po_number' => $order->po_number,
+                        'id'                    => $order->order_id,
+                        'user_id'               => $order->user_id,
+                        'status'                => $order->status,
+                        'order_type'            => ($pre) ? Order::PRE_ORDER : Order::NORMAL,
+                        'platform'              => $order->location ? $order->location : ($order->online_order ? 'online' : 'backorder'),
+                        'amount'                => $order->total,
+                        'currency_id'           => 1,
+                        'tax_amount'            => 0,
+                        'shipping_amount'       => $order->shipping_cost,
+                        'description'           => $order->notes,
+                        'discount_amount'       => $order->subtotal_discount,
+                        'sub_total'             => $order->subtotal,
+                        'is_confirmed'          => 1,
+                        'is_finished'           => 1,
+                        'salesperson_id'        => $order->issuer_id,
+                        'po_number'             => $order->po_number,
                         'order_completion_date' => $order->complete_date,
-                        'created_at' => date('Y-m-d H:i:s', $order->timestamp),
-                        'updated_at' => date('Y-m-d H:i:s', $order->last_status_change_date),
+                        'created_at'            => date('Y-m-d H:i:s', $order->timestamp),
+                        'updated_at'            => date('Y-m-d H:i:s', $order->last_status_change_date),
                     ];
                     DB::table('ec_orders')->insert($orderData);
 
@@ -179,11 +179,11 @@ class fetchOrders extends Command
                             $diff = $orderProduct->amount - $looseQty;
                         }
                         $orderProductData = [
-                            'order_id' => $orderProduct->order_id,
-                            'qty' => $qty,
-                            'is_pack' => $isPack,
-                            'price' => $orderProduct->price,
-                            'product_id' => $orderProduct->product_id,
+                            'order_id'     => $orderProduct->order_id,
+                            'qty'          => $qty,
+                            'is_pack'      => $isPack,
+                            'price'        => $orderProduct->price,
+                            'product_id'   => $orderProduct->product_id,
                             'product_name' => $productObj ? $productObj->name : $orderProduct->product_code
                         ];
                         OrderProduct::create($orderProductData);
@@ -191,11 +191,11 @@ class fetchOrders extends Command
                         if ($diff) {
                             $isPack = 0;
                             $orderProductData = [
-                                'order_id' => $orderProduct->order_id,
-                                'qty' => $diff,
-                                'is_pack' => $isPack,
-                                'price' => $orderProduct->price,
-                                'product_id' => $orderProduct->product_id,
+                                'order_id'     => $orderProduct->order_id,
+                                'qty'          => $diff,
+                                'is_pack'      => $isPack,
+                                'price'        => $orderProduct->price,
+                                'product_id'   => $orderProduct->product_id,
                                 'product_name' => $productObj ? $productObj->name : $orderProduct->product_code
                             ];
                             OrderProduct::create($orderProductData);
@@ -204,66 +204,66 @@ class fetchOrders extends Command
                     }
 
                     $custAddB = [
-                        'name' => $order->b_firstname . ' ' . $order->b_lastname,
-                        'email' => $order->email,
-                        'phone' => $order->b_phone,
-                        'country' => $order->b_country,
-                        'state' => $order->b_state,
-                        'city' => $order->b_city,
-                        'address' => $order->b_address_2,
+                        'name'        => $order->b_firstname . ' ' . $order->b_lastname,
+                        'email'       => $order->email,
+                        'phone'       => $order->b_phone,
+                        'country'     => $order->b_country,
+                        'state'       => $order->b_state,
+                        'city'        => $order->b_city,
+                        'address'     => $order->b_address_2,
                         'customer_id' => $order->user_id,
-                        'zip_code' => $order->b_zipcode,
-                        'first_name' => $order->b_firstname,
-                        'last_name' => $order->b_lastname,
-                        'company' => $order->company,
-                        'type' => 'billing',
+                        'zip_code'    => $order->b_zipcode,
+                        'first_name'  => $order->b_firstname,
+                        'last_name'   => $order->b_lastname,
+                        'company'     => $order->company,
+                        'type'        => 'billing',
                     ];
                     $custBA = Address::create($custAddB);
 
                     $billingAddress = [
-                        'name' => $order->b_firstname . ' ' . $order->b_lastname,
-                        'phone' => $order->b_phone,
-                        'email' => $order->email,
-                        'country' => $order->b_country,
-                        'state' => $order->b_state,
-                        'city' => $order->b_city,
-                        'address' => $order->b_address_2,
-                        'order_id' => $order->order_id,
-                        'zip_code' => $order->b_zipcode,
-                        'type' => 'billing',
+                        'name'                => $order->b_firstname . ' ' . $order->b_lastname,
+                        'phone'               => $order->b_phone,
+                        'email'               => $order->email,
+                        'country'             => $order->b_country,
+                        'state'               => $order->b_state,
+                        'city'                => $order->b_city,
+                        'address'             => $order->b_address_2,
+                        'order_id'            => $order->order_id,
+                        'zip_code'            => $order->b_zipcode,
+                        'type'                => 'billing',
                         'customer_address_id' => $custBA->id,
                     ];
                     OrderAddress::create($billingAddress);
 
 
                     $custAddS = [
-                        'name' => $order->s_firstname . ' ' . $order->s_lastname,
-                        'email' => $order->email,
-                        'phone' => $order->s_phone,
-                        'country' => $order->s_country,
-                        'state' => $order->s_state,
-                        'city' => $order->s_city,
-                        'address' => $order->s_address_2,
+                        'name'        => $order->s_firstname . ' ' . $order->s_lastname,
+                        'email'       => $order->email,
+                        'phone'       => $order->s_phone,
+                        'country'     => $order->s_country,
+                        'state'       => $order->s_state,
+                        'city'        => $order->s_city,
+                        'address'     => $order->s_address_2,
                         'customer_id' => $order->user_id,
-                        'zip_code' => $order->s_zipcode,
-                        'first_name' => $order->s_firstname,
-                        'last_name' => $order->s_lastname,
-                        'company' => $order->company,
-                        'type' => 'shipping',
+                        'zip_code'    => $order->s_zipcode,
+                        'first_name'  => $order->s_firstname,
+                        'last_name'   => $order->s_lastname,
+                        'company'     => $order->company,
+                        'type'        => 'shipping',
                     ];
                     $custSA = Address::create($custAddS);
 
                     $shippingAddress = [
-                        'name' => $order->s_firstname . ' ' . $order->s_lastname,
-                        'phone' => $order->s_phone,
-                        'email' => $order->email,
-                        'country' => $order->s_country,
-                        'state' => $order->s_state,
-                        'city' => $order->s_city,
-                        'address' => $order->s_address_2,
-                        'order_id' => $order->order_id,
-                        'zip_code' => $order->s_zipcode,
-                        'type' => 'shipping',
+                        'name'                => $order->s_firstname . ' ' . $order->s_lastname,
+                        'phone'               => $order->s_phone,
+                        'email'               => $order->email,
+                        'country'             => $order->s_country,
+                        'state'               => $order->s_state,
+                        'city'                => $order->s_city,
+                        'address'             => $order->s_address_2,
+                        'order_id'            => $order->order_id,
+                        'zip_code'            => $order->s_zipcode,
+                        'type'                => 'shipping',
                         'customer_address_id' => $custSA->id,
                     ];
                     OrderAddress::create($shippingAddress);
