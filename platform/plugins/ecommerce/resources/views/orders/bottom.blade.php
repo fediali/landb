53fcda7e0469eddfb8e1c21795920aba802339e5
Fedi <farhad.ali@landbapparel.com>
2021-11-18 16:10:01 -0600

    #ff

--- platform/plugins/ecommerce/resources/views/orders/bottom.blade.php
+++ platform/plugins/ecommerce/resources/views/orders/bottom.blade.php
@@ -2,9 +2,12 @@
     <p>Gross Total : $ {{number_format($data['gross_total'])}}</p>
     <p class="total-green">Total Paid : <span>$ {{number_format($data['total_paid'])}}</span></p>
     <p>Total Shipping Cost : $ {{number_format($data['total_shipping_cost'])}}</p>
-    <p>Discount : $ {{number_format($data['discount'])}}</p>
+    @if($data['discount'])
+        <p>Discount : $ {{number_format($data['discount'])}}</p>
+    @endif()
 </div>
 
 <script>
-    $(document).ready(function () {});
+    $(document).ready(function () {
+    });
 </script>
