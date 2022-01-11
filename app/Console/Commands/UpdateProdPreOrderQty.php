f4d612d193ef7438bf7d68ae37d4a92504e4c976
Fedi <farhad.ali@landbapparel.com>
2022-01-11 12:13:53 -0600

    #ff

--- app/Console/Commands/UpdateProdPreOrderQty.php
+++ app/Console/Commands/UpdateProdPreOrderQty.php
@@ -34,34 +34,38 @@ class UpdateProdPreOrderQty extends Command
 
     public function handle()
     {
-        DB::connection('mysql2')
+        $products = DB::connection('mysql2')
             ->table('hw_products')
-            //->where('hw_products.product_id', 114388)
-            ->orderBy('hw_products.product_id', 'ASC')
-            ->chunk(500, function ($products) {
-                foreach ($products as $product) {
-                    $row = (array)$product;
-                    if ($row['product_id']) {
-                        $product = DB::connection('mysql2')
-                            ->table('hw_order_details')
-                            ->selectRaw('COUNT(hw_order_details.order_id) AS sum_order')
-                            ->selectRaw('SUM(hw_order_details.amount) AS sum_quantity')
-                            ->join('hw_orders', 'hw_orders.order_id', 'hw_order_details.order_id')
-                            ->where('hw_orders.status', 'AZ')
-                            ->where('hw_order_details.product_id', $row['product_id'])
-                            ->groupBy('hw_order_details.product_code')
-                            ->first();
-
-                        if($product){
-                            DB::connection('mysql2')
-                                ->table('hw_products')
-                                ->where('hw_products.product_id', $row['product_id'])
-                                ->update(['quantity_preorder' => $product->sum_quantity, 'count_preorder' => $product->sum_order]);
-                        }
-
-                    }
+            ->where('hw_products.product_id', 83702)->get();
+//            ->orderBy('hw_products.product_id', 'ASC')
+//            ->chunk(500, function ($products) {
+        foreach ($products as $product) {
+            $row = (array)$product;
+//            dd($row['product_id']);
+            if ($row['product_id']) {
+                $product = DB::connection('mysql2')
+                    ->table('hw_order_details')
+                    ->selectRaw('COUNT(hw_order_details.order_id) AS sum_order')
+                    ->selectRaw('SUM(hw_order_details.amount) AS sum_quantity')
+                    ->join('hw_orders', 'hw_orders.order_id', 'hw_order_details.order_id')
+                    ->where(function($q){
+                        $q->where('hw_orders.status', 'AR');
+                        $q->orWhere('hw_orders.status', 'AZ');
+                        $q->orWhere('hw_orders.status', 'B');
+                    })
+                    ->where('hw_order_details.product_id', $row['product_id'])
+                    ->groupBy('hw_order_details.product_code')
+                    ->first();
+                if ($product) {
+                    DB::connection('mysql2')
+                        ->table('hw_products')
+                        ->where('hw_products.product_id', $row['product_id'])
+                        ->update(['quantity_preorder' => $product->sum_quantity, 'count_preorder' => $product->sum_order]);
                 }
-            });
+
+            }
+        }
+//            });
         echo 'success';
     }
 }
