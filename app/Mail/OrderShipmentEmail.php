18bfe4652927fb4a67b99fce7920a713a666cf61
shakirmirza-bv <shakir@bargoventures.com>
2022-02-12 03:28:15 +0500

    order shipment email

--- app/Mail/OrderShipmentEmail.php
+++ app/Mail/OrderShipmentEmail.php
@@ -32,8 +32,10 @@ class OrderShipmentEmail extends Mailable
     {
         $products = DB::connection('mysql2')
             ->table('hw_order_details')
-            ->where('order_id', $this->data['order_id'])
-            ->whereIn('product_id', $this->data['order_product_ids'])
+            ->select('hw_order_details.*', 'hw_product_descriptions.product AS product_name')
+            ->join('hw_product_descriptions', 'hw_product_descriptions.product_id', 'hw_order_details.product_id')
+            ->where('hw_order_details.order_id', $this->data['order_id'])
+            ->whereIn('hw_order_details.product_id', $this->data['order_product_ids'])
             ->get();
 
         return $this->view('emails.order_shipment_email')->subject('Your Items has Shipped! - Lucky and Blessed')->with(['products' => $products]);
