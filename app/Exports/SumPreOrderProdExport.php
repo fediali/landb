d1a3a1d1a8646f37e8874b64bb9b4d76f153310e
shakirmirza-bv <shakir@bargoventures.com>
2022-03-01 02:07:24 +0500

    order shipment email cc

--- app/Exports/SumPreOrderProdExport.php
+++ app/Exports/SumPreOrderProdExport.php
@@ -19,8 +19,11 @@ class SumPreOrderProdExport implements FromCollection, ShouldAutoSize, WithHeadi
         $products = DB::connection('mysql2')
             ->table('hw_order_details')
             ->select('hw_order_details.product_code')
+            ->selectRaw("FROM_UNIXTIME(hw_products.avail_since, '%b %d, %Y') AS eta_from")
+            ->selectRaw("FROM_UNIXTIME(hw_products.avail_to, '%b %d, %Y') AS eta_to")
             ->selectRaw('SUM(hw_order_details.amount) AS sum_quantity')
             ->join('hw_orders', 'hw_orders.order_id', 'hw_order_details.order_id')
+            ->join('hw_products', 'hw_products.product_id', 'hw_order_details.product_id')
             //->where('hw_orders.status', 'AZ')
             ->where(function ($q) {
                 $q->where('hw_orders.status', 'AZ');
@@ -38,6 +41,8 @@ class SumPreOrderProdExport implements FromCollection, ShouldAutoSize, WithHeadi
         return [
             'Product Code',
             'Sum Quantity',
+            'Eta From',
+            'Eta To'
         ];
     }
 
@@ -48,7 +53,7 @@ class SumPreOrderProdExport implements FromCollection, ShouldAutoSize, WithHeadi
     {
         return [
             AfterSheet::class    => function(AfterSheet $event) {
-                $cellRange = 'A1:B1'; // All headers
+                $cellRange = 'A1:D1'; // All headers
                 $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setSize(16)->getBold();
             },
         ];
