6c1bdb003ac2b9beddbb9fd05d96e54be95bc5ba
faisalalidev <105174254+faisalalidev@users.noreply.github.com>
2023-02-27 01:37:03 +0500

    increase range limit

--- platform/plugins/textmessages/resources/views/addCustomerFields.blade.php
+++ platform/plugins/textmessages/resources/views/addCustomerFields.blade.php
@@ -12,6 +12,11 @@
                     <option value="100">100</option>
                     <option value="500">500</option>
                     <option value="1000">1000</option>
+                    <option value="2000">2000</option>
+                    <option value="3000">3000</option>
+                    <option value="4000">4000</option>
+                    <option value="5000">5000</option>
+                    <option value="10000">10000</option>
                 </select>
             </div>
         </div>
