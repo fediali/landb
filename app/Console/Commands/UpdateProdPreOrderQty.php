938ebef254b17710077858f346671234a2838eff
Fedi <farhad.ali@landbapparel.com>
2023-03-31 14:45:28 -0500

    #ff

--- app/Console/Commands/UpdateProdPreOrderQty.php
+++ app/Console/Commands/UpdateProdPreOrderQty.php
@@ -38,6 +38,7 @@ class UpdateProdPreOrderQty extends Command
             ->table('hw_products')
             ->where('hw_products.ptype', 'P')
             ->where('hw_products.product_id', 116458)->get();
+        dd($row);
 //            ->orderBy('hw_products.product_id', 'ASC')
 //            ->chunk(500, function ($products) {
 //                foreach ($products as $product) {
